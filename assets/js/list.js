e50057aa9aee8e4a3abadbe01c4f7b0daa1ccd98
Michał <m.majewski.dm@gmail.com>
2025-02-25 21:30:05 +0100

    added navigation list for article in JavaScript

--- assets/js/list.js
+++ assets/js/list.js
@@ -43,3 +43,46 @@ const list = [
         link: '#Osadzanie',
     }
 ];
+
+
+
+const ulListContainer = document.querySelector('.article__list');
+if(ulListContainer) {
+
+const ulElement = document.createElement('ul');
+
+list.forEach(listElement => {
+    if(listElement.parentId === null) {
+        const li = document.createElement('li');
+        li.dataset.id = listElement.id;
+        const linkElement = document.createElement('a');
+        linkElement.setAttribute('href', listElement.link);
+        linkElement.textContent = listElement.text;
+        li.appendChild(linkElement);
+
+       const children = list.filter(function(child) {
+            return Number(child.parentId) === Number(listElement.id)})
+
+        if(children.length > 0) {
+            const secondUL = document.createElement('ul');
+
+            children.forEach(childElement => {
+                const childLi = document.createElement('li');
+                const childLinkElement = document.createElement('a');
+                childLinkElement.setAttribute('href', childElement.link);
+                childLinkElement.textContent = childElement.text;
+                childLi.appendChild(childLinkElement);
+
+                secondUL.appendChild(childLi);
+            })
+
+            li.appendChild(secondUL);
+        }
+
+        ulElement.appendChild(li);
+
+    }
+})
+
+ulListContainer.appendChild(ulElement);
+}
